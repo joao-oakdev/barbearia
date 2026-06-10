e1b442d03fe9a81b540cbd994c1da3660ec9091e
Joao Carvalho <joao.oakdev@gmail.com>
2026-06-10 15:35:56 -0300

    chore: changed api.ts to use frontend on vercel and localhost if needed

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api'
+    // Lê a variável da Vercel em produção, mas mantém o localhost para o seu desenvolvimento
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api'
 });
 
 // Interceptor de REQUEST — adiciona o token
